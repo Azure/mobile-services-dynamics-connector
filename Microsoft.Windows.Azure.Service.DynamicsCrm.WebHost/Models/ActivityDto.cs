c2fc68b615a39767d24668190f13915c0a9cc28e
Nathen Drees <ndrees@sonomapartners.com>
2015-03-03 14:09:28 -0600

    Added contact and activity controllers

---
+++ Microsoft.Windows.Azure.Service.DynamicsCrm.WebHost/Models/ActivityDto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.WindowsAzure.Mobile.Service.Tables;
+using System;
+
+namespace Microsoft.Windows.Azure.Service.DynamicsCrm.WebHost.Models
+{
+    public class ActivityDto : ITableData
+    {
+        public DateTimeOffset? CreatedAt { get; set; }
+        public bool Deleted { get; set; }
+        public string Id { get; set; }
+        public DateTimeOffset? UpdatedAt { get; set; }
+        public byte[] Version { get; set; }
+    }
+}
---
+++ Microsoft.Windows.Azure.Service.DynamicsCrm/DynamicsTableController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.WindowsAzure.Mobile.Service;
+using Microsoft.WindowsAzure.Mobile.Service.Tables;
+using Microsoft.Xrm.Client.Services;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Windows.Azure.Service.DynamicsCrm
+{
+    public abstract class DynamicsTableController<TDTO, TEntity> : TableController<TDTO>
+        where TDTO : class, ITableData
+        where TEntity : Entity
+    {
+        public DynamicsTableController() : this("CrmConnection")
+        {
+        }
+
+        public DynamicsTableController(string connectionStringName)
+            : this(new DynamicsCrmDomainManager<TDTO, TEntity>(new OrganizationService(connectionStringName)))
+        {
+        }
+
+        public DynamicsTableController(IDomainManager<TDTO> domainManager) : base(domainManager)
+        {
+        }
+    }
+}
