be79145785d64aea9909133265f1db6c311f671b
Corey O'Brien <cobrien@sonomapartners.com>
2015-03-09 15:29:44 -0500

    Update GenerateEntityTypes.bat
    Removed org details.

--- GenerateEntityTypes.bat
+++ GenerateEntityTypes.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 
-.\Tools\CrmSvcUtil\CrmSvcUtil /url:https://sonomap.crm.dynamics.com/XRMServices/2011/Organization.svc /username:developer@sonomap.onmicrosoft.com /password:$0n0m@p! /namespace:Microsoft.Windows.Azure.Service.DynamicsCrm.WebHost.Models /out:.\Microsoft.Windows.Azure.Service.DynamicsCrm.WebHost\Models\Entities.cs
+.\Tools\CrmSvcUtil\CrmSvcUtil /url:https://ORGNAME.crm.dynamics.com/XRMServices/2011/Organization.svc /username:USERNAME /password:PASSWORD /namespace:Microsoft.Windows.Azure.Service.DynamicsCrm.WebHost.Models /out:.\Microsoft.Windows.Azure.Service.DynamicsCrm.WebHost\Models\Entities.cs
