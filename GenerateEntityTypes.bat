288763f3614abb6d1b332bc3afcd1385361aec1d
Corey O'Brien <cobrien@sonomapartners.com>
2015-03-09 16:32:35 -0500

    Updated batch file to reflect new folder names.

--- GenerateEntityTypes.bat
+++ GenerateEntityTypes.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 
-.\Tools\CrmSvcUtil\CrmSvcUtil /url:https://ORGNAME.crm.dynamics.com/XRMServices/2011/Organization.svc /username:USERNAME /password:PASSWORD /namespace:Microsoft.Windows.Azure.Service.DynamicsCrm.WebHost.Models /out:.\Microsoft.Windows.Azure.Service.DynamicsCrm.WebHost\Models\Entities.cs
+.\Tools\CrmSvcUtil\CrmSvcUtil /url:https://ORGNAME.crm.dynamics.com/XRMServices/2011/Organization.svc /username:USERNAME /password:PASSWORD /namespace:Microsoft.WindowsAzure.Mobile.Service.DynamicsCrm.WebHost.Models /out:.\Microsoft.WindowsAzure.Mobile.Service.DynamicsCrm.WebHost\Models\Entities.cs
