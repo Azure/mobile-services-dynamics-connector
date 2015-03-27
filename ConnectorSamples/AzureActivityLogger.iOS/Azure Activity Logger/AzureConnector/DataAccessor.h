9f1c4981722677adf36cff6ca8b5d47d8b9b1f9e
Andrew Monshizadeh <amonshiz@gmail.com>
2015-03-27 11:56:35 -0500

    Remove recent contacts functionality.

--- ConnectorSamples/AzureActivityLogger.iOS/Azure Activity Logger/AzureConnector/DataAccessor.h
+++ ConnectorSamples/AzureActivityLogger.iOS/Azure Activity Logger/AzureConnector/DataAccessor.h
@@ -7,13 +7,9 @@
 
 #import <CoreData/CoreData.h>
 
-@class Contact;
-
 @interface DataAccessor : NSObject
 
 + (DataAccessor *)sharedAccessor;
-
-- (void)addContactToRecents:(Contact *)contact;
 
 /**
  * The following methods are intended to simplify the creation of
@@ -21,8 +17,6 @@
  * `FetchedResultsDataSource` object.
  */
 - (NSFetchedResultsController *)fetchedResultsControllerForObject:(NSString *)object withPredicate:(NSPredicate *)predicate sortDescriptors:(NSArray *)descriptors;
-/// Convenience method for getting recent contacts.
-- (NSFetchedResultsController *)recentContactsFetchedResultsController;
 /// Convenience method for getting all contacts.
 - (NSFetchedResultsController *)contactsFetchedResultsController;
 
