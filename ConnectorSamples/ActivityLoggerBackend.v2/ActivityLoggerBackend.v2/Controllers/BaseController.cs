efef3ee049b0a451eb968e4294530f2d71a0687b
Donna Malayeri <donnam@microsoft.com>
2015-03-30 22:23:11 -0700

    Add sample .net backend project for v2 in ActivityLoggerBackend.v2

---
+++ ConnectorSamples/ActivityLoggerBackend.v2/ActivityLoggerBackend.v2/Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Azure.Mobile.Server.DynamicsCrm;
+using Microsoft.Azure.Mobile.Server.Tables;
+using Microsoft.Xrm.Sdk;
+
+namespace ActivityLoggerBackend.Controllers
+{
+    public abstract class BaseController<TDto, TEntity> : DynamicsCrmTableController<TDto, TEntity>
+        where TDto : class, ITableData
+        where TEntity : Entity
+    {
+        public BaseController()
+            : this(false)
+        {
+        }
+
+        public BaseController(bool enableSoftDelete)
+            : base(new AutoMapperEntityMapper<TDto, TEntity>(enableSoftDelete))
+        {
+
+        }
+    }
+}
---
+++ ConnectorSamples/ActivityLoggerBackend.v2/ActivityLoggerBackend.v2/Controllers/ContactController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using ActivityLoggerBackend.Models;
+using Microsoft.Azure.Mobile.Server.Security;
+using Microsoft.Xrm.Client.Services;
+using Microsoft.Xrm.Sdk.Client;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.OData.Query;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.WebServiceClient;
+using Microsoft.Xrm.Client;
+using System.Net;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Azure.Mobile.Security;
+
+namespace ActivityLoggerBackend.Controllers
+{
+    [AuthorizeLevel(AuthorizationLevel.User)]
+    public class ContactController : BaseController<ContactDto, Contact>
+    {
+        public ContactController() : base(true)
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<ContactDto>> Get(ODataQueryOptions<ContactDto> query)
+        {
+            return await QueryAsync(query, qe => qe.Criteria.AddCondition("ownerid", ConditionOperator.EqualUserId));
+        }
+    }
+}
